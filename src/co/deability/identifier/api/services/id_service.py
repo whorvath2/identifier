7963ad75dea624381e31578a87f76d33f7f0ce51
Bill Horvath <bhorvath@umich.edu>
2022-05-07 14:32:43 -0400

    Added functions for creating new ids and adding data that can be called at the id_service module level, and which use a default IdRepository writer instance if none is supplied as a parameter. Modified the IdCreator to delegate its corresponding methods to the new module-level functions.

--- src/co/deability/identifier/api/services/id_service.py
+++ src/co/deability/identifier/api/services/id_service.py
@@ -13,7 +13,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from typing import Any
+from typing import Any, Final
 
 from co.deability.identifier import config
 from co.deability.identifier.api.repositories.id_repository import (
@@ -23,33 +23,24 @@ from co.deability.identifier.api.repositories.id_repository import (
 from co.deability.identifier.errors.BadRequestError import BadRequestError
 from co.deability.identifier.errors.IllegalArgumentError import IllegalArgumentError
 
+WRITER_REPOSITORY: Final[IdRepository] = IdRepository(
+    repository_type=IdRepositoryType.WRITER
+)
 
-class IdCreator:
-    def __init__(
-        self,
-        id_repository: IdRepository = IdRepository(
-            repository_type=IdRepositoryType.WRITER
-        ),
-    ):
-        if not id_repository.get_type() == IdRepositoryType.WRITER:
-            raise IllegalArgumentError(
-                message="The supplied repository is not a supported type."
-            )
-        self.id_repository = id_repository
 
-    def get_new_id(self) -> dict:
-        return {
-            "created": self.id_repository.create_id(retries=config.MAX_WRITE_RETRIES)
-        }
+def create_new_id(id_repository: IdRepository = WRITER_REPOSITORY) -> dict:
+    return {"created": id_repository.create_id(retries=config.MAX_WRITE_RETRIES)}
 
-    def add_data(self, data: dict[str, Any], identifier: str) -> dict[str, Any]:
-        if not data:
-            raise BadRequestError(
-                message="The data is missing from the body of the request."
-            )
-        return self.id_repository.add_data(
-            data=data, identifier=identifier
-        ).get_current_data(identifier=identifier)
+
+def add_data(
+    data: dict[str, Any],
+    identifier: str,
+    id_repository: IdRepository = WRITER_REPOSITORY,
+) -> dict[str, Any]:
+    if not data:
+        raise BadRequestError(message="The data is missing or empty.")
+    id_repository.add_data(data=data, identifier=identifier)
+    return get_current_data(identifier=identifier)
 
 
 def get_current_data(identifier: str) -> dict[str, Any]:
@@ -62,9 +53,35 @@ def get_current_data(identifier: str) -> dict[str, Any]:
 def get_all_data(identifier: str) -> dict[str, Any]:
     return {f"{identifier}": _get_reader().get_all_data(identifier=identifier)}
 
+
 def exists(identifier: str) -> dict:
     return {f"{identifier} exists": _get_reader().exists(identifier=identifier)}
 
 
 def _get_reader() -> IdRepository:
     return IdRepository(repository_type=IdRepositoryType.READER)
+
+
+class IdCreator:
+    """
+    Service class that supports use of a specific IdRepository instance, which must be an
+    IdRepositoryType.WRITER type.
+    """
+
+    def __init__(
+        self,
+        id_repository: IdRepository = WRITER_REPOSITORY,
+    ):
+        if not id_repository.get_type() == IdRepositoryType.WRITER:
+            raise IllegalArgumentError(
+                message="The supplied repository is not a supported type."
+            )
+        self.id_repository = id_repository
+
+    def get_new_id(self) -> dict:
+        return create_new_id(id_repository=self.id_repository)
+
+    def add_data(self, data: dict[str, Any], identifier: str) -> dict[str, Any]:
+        return add_data(
+            data=data, identifier=identifier, id_repository=self.id_repository
+        )
